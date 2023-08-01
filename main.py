0896ae7b9af19af6b2d6735ac6617777edffbdac
yusuf202021 <57948422+yusuf202021@users.noreply.github.com>
2023-08-01 17:06:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,122 @@
+import json
+
+import requests, uuid
+from rich import pretty, print
+import signing
+pretty.install()
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class Reddit:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.device_id = str(uuid.uuid4())
+        self.adversary_id = str(uuid.uuid4())
+        self.x_reddit_loid = ""
+        self.x_reddit_session = ""
+        self.user_agent = 'Reddit/Version 2023.29.0/Build 1059855/Android 12'
+        self.session.verify = False
+
+
+
+        self.get_basic_access_token()
+        self.verify_token()
+
+    def register(self, e_mail : str, username : str, password : str):
+
+        json_data = {
+          "username": username,
+          "password": password,
+          "email": e_mail,
+          "newsletter_subscribe": False
+        }
+
+        body = json.dumps(json_data)
+
+        epoch = signing.current_epoch()
+
+        hmac_header = signing.get_hmac_header(epoch, body, self.user_agent, self.device_id)
+
+        register_response = self.session.post(
+            "https://accounts.reddit.com/api/register",
+            headers= {
+                'accept-encoding': 'gzip',
+                'client-vendor-id': self.device_id,
+                'connection': 'Keep-Alive',
+                'content-type': 'application/json; charset=UTF-8',
+                'host': 'accounts.reddit.com',
+                'user-agent': self.user_agent,
+                'x-hmac-signed-body': hmac_header.hmac_signed_body,
+                'x-hmac-signed-result': hmac_header.hmac_signed_result,
+                'x-reddit-compression': '1',
+                'x-reddit-loid': self.x_reddit_loid,
+                'x-reddit-media-codecs': 'available-codecs=video/avc, video/x-vnd.on2.vp9',
+                'x-reddit-qos': 'down-rate-mbps=3.200',
+                'x-reddit-retry': 'algo=no-retries',
+            },
+            data = body
+        )
+        assert register_response.ok, f"{self.__class__.__name__}.register(): {register_response.json().get('error', {}).get('explanation')}"
+
+    def verify_token(self):
+        verify_token_response = self.session.get(
+            "https://oauth.reddit.com/api/v1/me?raw_json=1&feature=link_preview&sr_detail=true&expand_srs=true&from_detail=true&api_type=json&raw_json=1&always_show_media=1",
+            headers= {
+                'accept-encoding': 'gzip',
+                'accept-language': 'en,en;q=0.9',
+                'authorization': self.token,
+                'client-vendor-id': self.device_id,
+                'connection': 'Keep-Alive',
+                'device-name': 'Google;sdk_gphone64_x86_64',
+                'host': 'oauth.reddit.com',
+                'user-agent': self.user_agent,
+                'x-dev-ad-id': self.adversary_id,
+                'x-reddit-compression': '1',
+                'x-reddit-device-id': self.device_id,
+                'x-reddit-dpr': '2.625',
+                'x-reddit-loid': self.x_reddit_loid,
+                'x-reddit-media-codecs': 'available-codecs=video/avc, video/x-vnd.on2.vp9',
+                'x-reddit-qos': 'down-rate-mbps=3.200',
+                'x-reddit-retry': 'attempt=0, max=3, algo=full-jitter',
+                'x-reddit-width': '1080',
+            }
+        )
+
+        assert verify_token_response.ok, f"{self.__class__.__name__}.verify_token()"
+        self.x_reddit_session = verify_token_response.headers.get("x-reddit-session", self.x_reddit_session)
+
+    def get_basic_access_token(self) -> str:
+        access_token_response = self.session.post(
+            "https://accounts.reddit.com/api/access_token",
+            headers = {
+                'accept-encoding': 'gzip',
+                'authorization': 'Basic b2hYcG9xclpZdWIxa2c6',
+                'client-vendor-id': self.device_id,
+                'connection': 'Keep-Alive',
+                'content-type': 'application/json; charset=UTF-8',
+                'host': 'accounts.reddit.com',
+                'user-agent': self.user_agent,
+                'x-reddit-compression': '1',
+                'x-reddit-media-codecs': 'available-codecs=video/avc, video/x-vnd.on2.vp9',
+                'x-reddit-qos': 'down-rate-mbps=3.200',
+                'x-reddit-retry': 'algo=no-retries',
+            },
+            json = {
+                "scopes": [
+                    "*",
+                    "email",
+                    "pii"
+              ]
+            }
+        )
+        assert access_token_response.status_code == 200, access_token_response.text
+        self.x_reddit_loid = access_token_response.headers.get("x-reddit-loid", self.x_reddit_loid)
+        self.token = f"{access_token_response.json().get('token_type').title()} {access_token_response.json().get('access_token')}"
+        return self.token
+
+reddit = Reddit()
+reddit.register("email@gmail.com", "username", "124124wrqrqwr")
+print(reddit.get_basic_access_token())
---
+++ signing.py
@@ -0,0 +1,46 @@
+import time, hmac, hashlib
+from dataclasses import dataclass
+
+MAP = b'\xd1\t\xcbc\x164#\xe6\xcf\x90\xf1\x18-\xb9\x0f,\xb5\x00\x8a\x1d\xf2\x06\x9bQ\xd6\xa4e>\xd5\xac\x07Tn\xadvi@\x88r\x95\x98\x1f\xf8Y\xeaoP\xa3\x12U\x87p\xc2\xd4\x01x\x10a\x84]\xd0\x82L\x03\x17\xc0\xf5\xf3_\xa5B\x971*\xa8G\\M\x93\x02$d0\x8d!\xc1+\xe8S\x83\x85I\xa2\x117\xbeH\xb6\xf6\x9e&5\x04\x0egk\xc3\xd9\xdd\x9f\xeb\x14~\xbas\x80\xb8\xbd\xfa\xcaw\xd7\xc8u\xe2\xbb\xe3\x1c\x966\xa9\r\xaa?\xc7\xb1b8\x0b\xfc\xa0\'{\xb2\xe5\xfb\xc9\x15q\xf4\x1b\xfeD.Zy\xa7\x19\xfd;\x8b"\xa1\xec\xdffJ\xed \x86=l^\x99C\xaf\xe1\n\xef\xd3\x13\xde\x94h\xb4\xcdj\xdc\xff<}\xe7\x91)%\xc53mz9\xe4:O\x1e\xdbAVN\x9c\x81\xe9\xb7\xbf\xe0\x9aR\xc4\x8c\x7f\x0cW/\xabt\x1a\x92EF\xceK\xbc\xf0\x89\xf9\xb0\xb3\xd2X\x08\xcc\xd8\xc6\xda2\xa6(\xee[\x8f\x05\xae\x9d`\x8e\xf7|'
+
+KEY = b'\x89\x03^9\x8899e\xa5\xc7Re\xa5^R\x05RR\x03\x89H\x88\xa5\xc49H9HRH\x1a^e\xa5^\xf3HRHR\x05\x88H\xc7\xc7H\xa5R\x03Qe\xf3\x05R99\x89R\x03\x05Q\xc7\xc7Q\x11'
+
+SIGNING_KEY = b'8c7abaa5f905f70400c81bf3a1a101e75f7210104b1991f0cd5240aa80c4d99d'
+
+@dataclass
+class HMACHeader:
+    hmac_signed_body: str
+    hmac_signed_result: str
+    epoch: int
+
+
+def current_epoch():
+    milliseconds = int(round(time.time() * 1000))
+    return milliseconds // 1000
+
+def hash(key, data):
+    secret_key_spec = hmac.new(key, digestmod=hashlib.sha256)
+    mac = hmac.new(key, data, hashlib.sha256)
+    do_final = mac.digest()
+    return do_final.hex()
+
+def format_epoch_body(epoch: int, body: str):
+    return f"Epoch:{epoch}|Body:{body}"
+
+def format_epoch_useragent_client_vendor_id(epoch, useragent, client_vendor_id):
+    return f"Epoch:{epoch}|User-Agent:{useragent}|Client-Vendor-ID:{client_vendor_id}"
+
+def format_hash_as_header(hash, epoch):
+    return f"1:android:2:{epoch}:{hash}"
+
+
+def get_hmac_header(epoch:int, body: str, user_agent: str, device_id: str):
+    hmac_signed_body = format_hash_as_header(hash(SIGNING_KEY, format_epoch_body(epoch, body).encode()),
+                                             epoch)
+    hmac_signed_result = format_hash_as_header(
+        hash(SIGNING_KEY, format_epoch_useragent_client_vendor_id(epoch, user_agent, device_id).encode()),
+        epoch)
+    return HMACHeader(hmac_signed_body, hmac_signed_result, epoch)
+
+
+
